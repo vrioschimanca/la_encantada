60b96ad398ba3403e1ca79397cff688078bbb64d
vrioschimanca <vrioschimanca@gmail.com>
2024-07-30 12:28:08 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+jQuery(function($){
+	
+	$(document).ready(function() {
+		if(cur_page_data.pageid == 1){
+			$('.header_lower_row' ).each(function () {
+				this.style.setProperty( 'position', 'relative', 'important');
+			});			
+		}
+		jQuery('.counter-item').appear(function() {
+			jQuery('.counter-number').countTo();
+			jQuery(this).addClass('funcionando');
+			console.log('funcionando');
+		});
+		
+	}); 
+	
+});
